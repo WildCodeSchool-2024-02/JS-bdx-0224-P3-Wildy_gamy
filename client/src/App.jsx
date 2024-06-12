a91bbfa67f39944e2c50d5405b4a5c02de043ef1
FGAT3 <feliciengamelin@yahoo.fr>
2024-06-12 16:32:53 +0200

    navbar finished but waiting img with express static

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import "./scss/index.scss";
 import NavBar from "./components/NavBar/NavBar";
 
 function App() {
--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -6,26 +6,26 @@ function NavBar() {
     <nav className="navbar-bottom" aria-label="Barre de navigation">
       <ul className="navbar-list">
         <li className="navbar-item">
+          <img className="icon-footer" src="" alt="" />
           <Link to="/" className="navbar-link">
-            <img className="icon-footer" src="" alt="" />
             Accueil
           </Link>
         </li>
         <li className="navbar-item">
+          <img className="icon-footer catalog-icon" src="" alt="" />
           <Link to="/catalog" className="navbar-link">
-            <img className="icon-footer catalog-icon" src="" alt="" />
             Catalogue
           </Link>
         </li>
         <li className="navbar-item">
+          <img className="icon-footer" src="" alt="" />
           <Link to="/demo" className="navbar-link">
-            <img className="icon-footer" src="" alt="" />
             Démo
           </Link>
         </li>
         <li className="navbar-item">
+          <img className="icon-footer" src="" alt="" />
           <Link to="/reward" className="navbar-link">
-            <img className="icon-footer" src="" alt="" />
             Prix
           </Link>
         </li>
