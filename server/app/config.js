d7798f3fa6f456f486b56444483cb0f9fa0d2a35
pjroggy <pierrejeanroggy@gmail.com>
2024-06-13 11:14:28 +0200

    restart work

--- server/app/config.js
+++ server/app/config.js
@@ -85,6 +85,7 @@ app.use(
 /* ************************************************************************* */
 
 // Import the API router
+const path = require("path");
 const apiRouter = require("./routers/api/router");
 
 // Mount the API router under the "/api" endpoint
@@ -107,8 +108,7 @@ app.use("/api", apiRouter);
 // 1. Uncomment the lines related to serving static files and redirecting unhandled requests.
 // 2. Ensure that the `reactBuildPath` points to the correct directory where your client's build artifacts are located.
 
-/*
-const path = require("path");
+
 
 const reactBuildPath = path.join(__dirname, "/../../client/dist");
 const publicFolderPath = path.join(__dirname, "/../public");
@@ -126,7 +126,7 @@ app.get("*.*", express.static(publicFolderPath, { maxAge: "1y" }));
 app.get("*", (_, res) => {
   res.sendFile(path.join(reactBuildPath, "/index.html"));
 });
-*/
+
 
 /* ************************************************************************* */
 
