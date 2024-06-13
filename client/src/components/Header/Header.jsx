4afce74e65c38583b813771f0742dcf8320ee6ca
FGAT3 <feliciengamelin@yahoo.fr>
2024-06-13 23:33:04 +0200

    header v1 & corrected import index scss navbar

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,11 +1,6 @@
 import { Link } from "react-router-dom";
 import "./Header.scss";
 
-const BASE_URL_LOGO = "http://localhost:3000/static/assets/images/logo";
-const BASE_URL_ICON = "http://localhost:3000/static/assets/images/icon";
-const logoWGWritten2 = `${BASE_URL_LOGO}/logo-written-wg2.svg`;
-const addUserIcon = `${BASE_URL_ICON}/icons8-add-user.png`;
-
 function Header() {
   return (
     <header className="top-page">
@@ -16,17 +11,14 @@ function Header() {
         <Link to="/prix" className="nav-link">
           Prix
         </Link>
-        <Link to="/" className="link-logo-wg-written">
-          <img src={logoWGWritten2} alt="" className="logo-wg-written" />
+        <Link to="/" className="wg-written">
+          Wildy Gamy
         </Link>
         <Link to="/demo" className="nav-link">
           Démo
         </Link>
         <Link to="/connection" className="nav-link">
           Compte
-        </Link>
-        <Link to="/connection">
-          <img src={addUserIcon} alt="" className="add-user-icon" />
         </Link>
       </nav>
     </header>
