9f369ac808d440549f91fd413a81a2f779687242
Jade Jolivet <jade.jolivet@gmail.com>
2024-06-13 14:00:50 +0200

    missing main.jsx added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,23 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
+import GameListPage from "./pages/GameListPage";
+import GameListModal from "./components/GameListModal"; 
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
   },
+  {
+    path: "/games", // Chemin pour afficher la liste des jeux
+    element: <GameListPage />, 
+  },
+  {
+    path: "/game/:id", // Chemin pour afficher la modale du jeu avec un identifiant
+    element: <GameListModal />, 
+  },
+
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
