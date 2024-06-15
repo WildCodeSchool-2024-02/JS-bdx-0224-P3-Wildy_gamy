1b6d9b770de264a238a93d51d3b41ce7e4ddb4f2
Jade Jolivet <jade.jolivet@gmail.com>
2024-06-15 11:46:06 +0200

    Final Fix with Express Static done

--- client/src/pages/GameListPage/GameListPage.jsx
+++ client/src/pages/GameListPage/GameListPage.jsx
@@ -2,6 +2,8 @@ import gameImg from "../../data/gameImg.json";
 import "../../scss/index.scss";
 import "./GameListPage.scss";
 
+const BASE_URL = import.meta.env.VITE_API_URL;
+
 function GameListPage() {
   return (
     <>
@@ -9,7 +11,7 @@ function GameListPage() {
       <section className="image-list">
         {gameImg.map((image) => (
           <button type="button" key={image.name} className="game-image">
-            <img src={image.src} alt={image.alt} />
+            <img src={`${BASE_URL}/${image.src}`} alt={image.alt} />
           </button>
         ))}
       </section>
