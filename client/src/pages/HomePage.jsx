1d50db8871f8ff49fadafd720777b7a4d58a0658
PAUL BOURY <paulboury17@gmail.com>
2024-06-16 19:43:19 +0200

    added everything directly in HomePage and some scss changes

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,10 +1,23 @@
-import ClickableLogo from "../components/ClickableLogo";
 import "../scss/index.scss";
+import "./HomePage.scss"
+import { Link } from "react-router-dom";
+import LogoSrc from "../assets/images/logo-wild-gamy-v3.svg"
+
 
 function HomePage() {
   return (
-    <ClickableLogo />
-  )
+    <>
+      <h1>
+        Rejoignez notre communauté de joueurs passionnés et découvrez nos bornes
+        d'arcade !
+      </h1>
+      <Link to="/Catalogue">
+        {" "}
+        <img src={LogoSrc} alt="aller à la page catalogue" />{" "}
+      </Link>
+      <p>Cliquez pour accéder à notre catalogue de jeux</p>
+    </>
+  );
 }
 
 export default HomePage;
