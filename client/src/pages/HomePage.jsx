5332b5897937fc2c5251aa9ac0c7be9e7f54808a
PAUL BOURY <paulboury17@gmail.com>
2024-06-17 11:48:37 +0200

    imported the logo from backend and made a few changes in the scscc

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,22 +1,25 @@
 import "../scss/index.scss";
 import "./HomePage.scss"
 import { Link } from "react-router-dom";
-import LogoSrc from "../assets/images/logo-wild-gamy-v3.svg"
+
+
+const BASE_URL = import.meta.env.VITE_API_URL;
+const logoWildyGamyWg = `${BASE_URL}/logo/logo-wildy-gamy-wg.svg`;
 
 
 function HomePage() {
   return (
-    <>
+    <section>
       <h1>
         Rejoignez notre communauté de joueurs passionnés et découvrez nos bornes
         d'arcade !
       </h1>
       <Link to="/Catalogue">
         {" "}
-        <img src={LogoSrc} alt="aller à la page catalogue" />{" "}
+        <img src={logoWildyGamyWg} alt="aller à la page catalogue" />{" "}
       </Link>
       <p>Cliquez pour accéder à notre catalogue de jeux</p>
-    </>
+    </section>
   );
 }
 
