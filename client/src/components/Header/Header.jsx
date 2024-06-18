70994477f8d1fff39a3468343eb55b9f38c999cc
FGAT3 <feliciengamelin@yahoo.fr>
2024-06-18 22:22:11 +0200

    refacto of refacto of refacto finished

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,42 +1,12 @@
-import { useLocation } from "react-router-dom";
-import NavLinkItem from "../NavLinkItem/NavLinkItem";
 import navLinks from "../../data/navLinks.json";
-import iconAddUser from "../../assets/images/icon/icons8-add-user.png";
 import "./Header.scss";
+import NavigationMenu from "../NavigationMenu/NavigationMenu";
 
 function Header() {
-  const location = useLocation();
-
-  const headerLinks = navLinks.header.map((link) => ({
-    ...link,
-    isActive: location.pathname === link.to,
-  }));
-
   return (
     <header className="top-page">
       <nav className="navigation">
-        {headerLinks.map((link) =>
-          link.to === "/connection" ? (
-            <NavLinkItem
-              key={link.to}
-              to={link.to}
-              label={link.label}
-              className={link.className}
-              icon={{
-                src: iconAddUser,
-                alt: "User Icon",
-                className: "account-icon",
-              }}
-            />
-          ) : (
-            <NavLinkItem
-              key={link.to}
-              to={link.to}
-              label={link.label}
-              className={link.className}
-            />
-          )
-        )}
+        <NavigationMenu links={navLinks.header} />
       </nav>
     </header>
   );
--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,29 +1,11 @@
-import { useLocation } from "react-router-dom";
-import NavLinkItem from "../NavLinkItem/NavLinkItem";
 import navLinks from "../../data/navLinks.json";
 import "./NavBar.scss";
+import NavigationMenu from "../NavigationMenu/NavigationMenu";
 
 function NavBar() {
-  const location = useLocation();
-
-  const navbarLinks = navLinks.navbar.map((link) => ({
-    ...link,
-    isActive: location.pathname === link.to,
-  }));
-
   return (
     <nav className="navbar-bottom" aria-label="Barre de navigation">
-      <ul className="navbar-list">
-        {navbarLinks.map((link) => (
-          <li key={link.to}>
-            <NavLinkItem
-              to={link.to}
-              label={link.label}
-              className={link.className}
-            />
-          </li>
-        ))}
-      </ul>
+      <NavigationMenu links={navLinks.navbar} />
     </nav>
   );
 }
