585074ed28d0a5c986536bc61c9226d706994a5d
FGAT3 <feliciengamelin@yahoo.fr>
2024-06-18 11:07:36 +0200

    refacto of refacto

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,35 +1,11 @@
-import { NavLink, useLocation } from "react-router-dom";
-import "./Header.scss";
+import Navigation from "../Navigation/Navigation";
 import navLinks from "../../data/navLinks.json";
-
-const BASE_URL = import.meta.env.VITE_API_URL;
+import "./Header.scss";
 
 function Header() {
-  const location = useLocation();
-
-  const isActive = (path) => location.pathname === path;
-
   return (
     <header className="top-page">
-      <nav className="navigation">
-        {navLinks.header.map((link) => (
-          <NavLink
-            key={link.to}
-            to={link.to}
-            className={`${link.className}`}
-            activeClassName="active"
-          >
-            {link.label}
-            {link.icon && (
-              <img
-                src={`${BASE_URL}/${link.icon.src}`}
-                alt={link.icon.alt}
-                className={`${link.icon.className} ${isActive(link.to) ? "active" : ""}`}
-              />
-            )}
-          </NavLink>
-        ))}
-      </nav>
+      <Navigation links={navLinks.header} className="navigation" />
     </header>
   );
 }
--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,23 +1,11 @@
-import { NavLink } from "react-router-dom";
-import "./NavBar.scss";
+import Navigation from "../Navigation/Navigation";
 import navLinks from "../../data/navLinks.json";
+import "./NavBar.scss";
 
 function NavBar() {
   return (
     <nav className="navbar-bottom" aria-label="Barre de navigation">
-      <ul className="navbar-list">
-        {navLinks.navbar.map((link) => (
-          <li key={link.to} className="navbar-item">
-            <NavLink
-              to={link.to}
-              className={`navbar-link ${link.className}`}
-              activeClassName="active"
-            >
-              {link.label}
-            </NavLink>
-          </li>
-        ))}
-      </ul>
+      <Navigation links={navLinks.navbar} className="navbar-list" />
     </nav>
   );
 }
