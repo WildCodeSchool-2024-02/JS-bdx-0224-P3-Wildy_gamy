d00d231322b8dfb3e8c430907bee48811a938a42
Jade Jolivet <jade.jolivet@gmail.com>
2024-06-25 16:21:43 +0200

    fix GameListModal.scss + GameRepository

--- server/database/models/GameRepository.js
+++ server/database/models/GameRepository.js
@@ -2,8 +2,6 @@ const AbstractRepository = require("./AbstractRepository");
 
 class GameRepository extends AbstractRepository {
   constructor() {
-    // Call the constructor of the parent class (AbstractRepository)
-    // and pass the table name "item" as configuration
     super({ table: "game" });
   }
 
@@ -31,21 +29,17 @@ class GameRepository extends AbstractRepository {
   // The Rs of CRUD - Read operations
 
   async read(id) {
-    // Execute the SQL SELECT query to retrieve a specific item by its ID
     const [rows] = await this.database.query(
       `select * from ${this.table} where id = ?`,
       [id]
     );
 
-    // Return the first row of the result, which represents the item
     return rows[0];
   }
 
   async readAll() {
-    // Execute the SQL SELECT query to retrieve all items from the "item" table
     const [rows] = await this.database.query(`select * from ${this.table}`);
 
-    // Return the array of items
     return rows;
   }
 
