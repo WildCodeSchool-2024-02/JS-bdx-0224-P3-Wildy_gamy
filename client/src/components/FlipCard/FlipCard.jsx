1ca46163485f68bc1d0cd3593d92f1f1d22f4523
FGAT3 <feliciengamelin@yahoo.fr>
2024-06-26 10:11:57 +0200

    create component flip card

---
+++ client/src/components/FlipCard/FlipCard.jsx
@@ -0,0 +1,45 @@
+import PropTypes from "prop-types";
+import "./FlipCard.scss";
+
+function FlipCard({ reward }) {
+  const BASE_URL = import.meta.env.VITE_API_URL;
+  const { name, image, description, price } = reward;
+
+  return (
+    <li className="flip-card">
+      <figure className="flip-card-inner">
+        <button
+          className="flip-card-front"
+          type="button"
+          aria-label={`Voir la description de la récompense ${name}`}
+        >
+          <img
+            src={`${BASE_URL}/reward/${image}`}
+            alt={`Illustration de la récompense ${name}`}
+            className="img-reward"
+          />
+        </button>
+        <button
+          className="flip-card-back"
+          type="button"
+          aria-label={`Voir l'image de la récompense ${name}`}
+        >
+          <figcaption>
+            {`${description} À échanger en salle contre ${price} ${price > 1 ? "jetons" : "jeton"}.`}
+          </figcaption>
+        </button>
+      </figure>
+    </li>
+  );
+}
+
+FlipCard.propTypes = {
+  reward: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+export default FlipCard;
--- client/src/pages/RewardPage/RewardPage.jsx
+++ client/src/pages/RewardPage/RewardPage.jsx
@@ -1,42 +1,19 @@
 import { useLoaderData } from "react-router-dom";
 import "./RewardPage.scss";
-
-const BASE_URL = import.meta.env.VITE_API_URL;
+import FlipCard from "../../components/FlipCard/FlipCard";
 
 function RewardPage() {
   const rewards = useLoaderData();
 
   return (
     <>
-      <h1>Liste des récompenses</h1>
+      <h1>
+        Jouez à une démo et atteignez un certain score pour obtenir des jetons
+        échangeables par des prix en boutique
+      </h1>
       <ul className="reward-list">
         {rewards.map((reward) => (
-          <li key={reward.id} className="flip-card">
-            <figure className="flip-card-inner">
-              <button
-                className="flip-card-front"
-                type="button"
-                aria-label={`Voir l'image de la récompense ${reward.name}`}
-              >
-                <img
-                  src={`${BASE_URL}/reward/${reward.image}`}
-                  alt={`Illustration de la récompense ${reward.name}`}
-                  className="img-reward"
-                />
-              </button>
-              <button
-                className="flip-card-back"
-                type="button"
-                aria-label={`Voir la description de la récompense ${reward.name}`}
-              >
-                <figcaption>
-                  <strong>{reward.name}</strong>: {reward.description}
-                  <p>Quantité disponible: {reward.remaining_quantity}</p>
-                  <p>Prix: {reward.price} jetons</p>
-                </figcaption>
-              </button>
-            </figure>
-          </li>
+          <FlipCard key={reward.id} reward={reward} />
         ))}
       </ul>
     </>
