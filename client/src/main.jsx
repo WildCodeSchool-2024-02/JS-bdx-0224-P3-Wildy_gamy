a0efcf274f646c04430382060a4efd94fef66a96
pjroggy <pierrejeanroggy@gmail.com>
2024-07-02 17:08:32 +0200

    add toast on form validation

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import {
   createBrowserRouter,
   redirect,
   RouterProvider,
 } from "react-router-dom";
-
+import { toast, ToastContainer } from "react-toastify";
 import App from "./App";
 import HomePage from "./pages/HomePage/HomePage";
 import GameListPage from "./pages/GameListPage/GameListPage";
@@ -16,13 +15,12 @@ import LoginPage from "./pages/LoginPage/LoginPage";
 import RegistrationPage from "./pages/RegistrationPage/RegistrationPage";
 import ErrorPage404 from "./pages/ErrorPage404/ErrorPage404";
 import { sendData, fetchApi } from "./services/api.service";
+import "react-toastify/dist/ReactToastify.css";
 
 const baseGamesUrl = "/api/games";
-
 const router = createBrowserRouter([
   {
     element: <App />,
-
     errorElement: <ErrorPage404 />,
     children: [
       { path: "/", element: <HomePage /> },
@@ -46,23 +44,19 @@ const router = createBrowserRouter([
           try {
             const formData = await request.formData();
             const data = Object.fromEntries(formData.entries());
-
             const { email, password } = data;
-
             if (!email || !password) {
               throw new Error("All fields are required");
             }
-
             const url = "/api/login";
-
             const response = await sendData(url, data, "POST");
-
             if (response.status === 200) {
+              toast.success("Login successful");
               return redirect(`/`);
             }
-
             throw new Error("Invalid response from server");
           } catch (error) {
+            toast.error(error.message);
             return { error: error.message };
           }
         },
@@ -74,23 +68,19 @@ const router = createBrowserRouter([
           try {
             const formData = await request.formData();
             const data = Object.fromEntries(formData.entries());
-
             const { firstname, lastname, pseudo, email, password } = data;
-
             if (!firstname || !lastname || !pseudo || !email || !password) {
               throw new Error("All fields are required");
             }
-
             const url = "/api/users";
-
             const response = await sendData(url, data, "POST");
-
             if (response.status === 201) {
+              toast.success("Registration successful");
               return redirect(`/connexion`);
             }
-
             throw new Error("Invalid response from server");
           } catch (error) {
+            toast.error(error.message);
             return { error: error.message };
           }
         },
@@ -98,11 +88,22 @@ const router = createBrowserRouter([
     ],
   },
 ]);
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <ToastContainer
+      position="top-right"
+      autoClose={3000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+      transition:Bounce
+    />
   </React.StrictMode>
 );
