c865c062ccb34db8e09d5406717dab5d1c817723
FGAT3 <feliciengamelin@yahoo.fr>
2024-07-07 18:10:28 +0200

    fixed text with scroll but no animation

--- client/src/pages/AboutUsPage/AboutUsPage.jsx
+++ client/src/pages/AboutUsPage/AboutUsPage.jsx
@@ -3,21 +3,19 @@ import logoWildyGamyWg from "../../assets/images/logo/logo-wildy-gamy.svg";
 
 function AboutUsPage() {
   return (
-    <article className="scrolling-content">
-      <img src={logoWildyGamyWg} alt="" />
-      <h1>Entrez dans le futur rétro chez Wildy Gamy !</h1>
-      <p>
-        Nous sommes plus qu’une simple salle d’arcade. Nous sommes une
-        passerelle vers le passé, un portail vers l’âge d’or des jeux vidéo. Nos
-        lumières au néon vous guideront vers des aventures palpitantes.
+    <>
+      <img src={logoWildyGamyWg} alt="" className="logo-about-wg" />
+      <p className="scrolling-text">
+        Entrez dans le futur rétro chez Wildy Gamy ! Nous sommes plus qu’une
+        simple salle d’arcade. Nous sommes une passerelle vers le passé, un
+        portail vers l’âge d’or des jeux vidéo. Nos lumières au néon vous
+        guideront vers des aventures palpitantes. Notre espace est imprégné de
+        l’esprit des années 80 et 90, avec des murs ornés d’affiches vintage,
+        des néons chatoyants et une ambiance électrisante. Nos bornes d’arcade
+        authentiques vous transportent dans des mondes pixelisés où les high
+        scores sont rois.
       </p>
-      <p>
-        Notre espace est imprégné de l’esprit des années 80 et 90, avec des murs
-        ornés d’affiches vintage, des néons chatoyants et une ambiance
-        électrisante. Nos bornes d’arcade authentiques vous transportent dans
-        des mondes pixelisés où les high scores sont rois.
-      </p>
-    </article>
+    </>
   );
 }
 
