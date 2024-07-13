6b23b50ca4f04cfffa787e12c29bb56b4d6e1f34
FGAT3 <feliciengamelin@yahoo.fr>
2024-07-13 15:30:56 +0200

    fixed game score

---
+++ client/src/services/score.service.js
@@ -0,0 +1,23 @@
+import { toast } from "react-toastify";
+import { sendData } from "./api.service";
+
+const sendScore = async (data) => {
+  try {
+    const { score, userId, gameId } = data;
+    if (!score || !userId || !gameId) {
+      throw new Error("All fields are required");
+    }
+    const url = "/api/parties";
+    const response = await sendData(url, data, "POST");
+    if (response.status === 201) {
+      toast.success("Votre score a bien été enregistré !");
+      return { success: true };
+    }
+    throw new Error("Erreur lors de l'envoi du score");
+  } catch (error) {
+    toast.error(error.message);
+    return { success: false };
+  }
+};
+
+export default sendScore;
